cf3c53004933bf1c9b9d121988572415680da8a1
Daqido55555 <daqido55@gmail.com>
2025-05-20 21:37:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,80 @@
 import discord
 from discord.ext import commands
-import os
-from keep_alive import keep_alive
-keep_alive()
+import asyncio
+import yt_dlp
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="Daq", intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+# Konfiguracja dla yt-dlp
+yt_opts = {
+    'format': 'bestaudio/best',
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'ytsearch',
+    'noplaylist': True,
+    'extract_flat': 'in_playlist'
+}
+
+FFMPEG_OPTIONS = {
+    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+    'options': '-vn'
+}
+
 
 @bot.event
 async def on_ready():
-    print(f"Zalogowano jako {bot.user}")
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send("Pong!")
-@bot.command()
-async def help(ctx):
-    embed = discord.Embed(
-        title="Commands",
-        description="Prefix: Daq/daq",
-        color=0xf5c211
-    )
-    embed.set_author(name="DaqBot")
-    embed.add_field(name="ping", value="Look your ping", inline=True)
-    embed.add_field(name="help", value="Bot commands", inline=True)
-    embed.add_field(name="play", value="Playing your love music", inline=True)
-    embed.set_footer(text="Help command")
-
-    await ctx.send(embed=embed)
+    print(f'Zalogowano jako {bot.user}')
 
 
+@bot.command(name='join')
+async def join(ctx):
+    if ctx.author.voice:
+        channel = ctx.author.voice.channel
+        await channel.connect()
+        await ctx.send("Dołączyłem do kanału głosowego.")
+    else:
+        await ctx.send("Musisz być na kanale głosowym!")
 
-bot.run(os.getenv("DISCORD_TOKEN"))
+
+@bot.command(name='leave')
+async def leave(ctx):
+    if ctx.voice_client:
+        await ctx.voice_client.disconnect()
+        await ctx.send("Rozłączono.")
+    else:
+        await ctx.send("Nie jestem na żadnym kanale głosowym.")
+
+
+@bot.command(name='play')
+async def play(ctx, *, search: str):
+    if not ctx.voice_client:
+        await ctx.invoke(join)
+
+    with yt_dlp.YoutubeDL(yt_opts) as ydl:
+        info = ydl.extract_info(search, download=False)
+        if 'entries' in info:
+            info = info['entries'][0]
+        url = info['url']
+
+    source = await discord.FFmpegOpusAudio.from_probe(url, **FFMPEG_OPTIONS)
+    ctx.voice_client.stop()
+    ctx.voice_client.play(source, after=lambda e: print(f'Odtwarzanie zakończone: {e}'))
+
+    await ctx.send(f"▶️ Odtwarzam: {info.get('title')}")
+
+
+@bot.command(name='stop')
+async def stop(ctx):
+    if ctx.voice_client:
+        ctx.voice_client.stop()
+        await ctx.send("⏹️ Zatrzymano odtwarzanie.")
+    else:
+        await ctx.send("Nie odtwarzam niczego.")
+
+
+# Wklej tutaj swój token bota
+bot.run('TWOJ_TOKEN_BOTA')
 
